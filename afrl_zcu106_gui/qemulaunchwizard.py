ea6d9a6c11ce4e5067c4b812995c0ef8b73b4714
B.J. Knabenshue <benjamin.j.knabenshue@nasa.gov>
2022-09-08 13:03:50 -0400

    removed unnecessary configuration items, added config file selection

--- afrl_zcu106_gui/qemulaunchwizard.py
+++ afrl_zcu106_gui/qemulaunchwizard.py
@@ -20,15 +20,12 @@ from PySide6.QtWidgets import QFileDialog, QWizard, QPlainTextEdit,QComboBox
 from PySide6.QtCore import Qt, Signal, Slot, QSize
 from PySide6.QtGui import QIcon,QIntValidator
 
-from afrl_zcu106_gui.common import RESOURCE_ROOT, QEMU_IMAGE_FILTERS, NETWORK_CFG
+from afrl_zcu106_gui.common import RESOURCE_ROOT, QEMU_IMAGE_FILTERS, QEMU_CFG_FILTERS, NETWORK_CFG
 from afrl_zcu106_gui.ui.ui_qemulaunchwizard import Ui_qemuLaunchWizard
 from afrl_zcu106_gui.qemuinstance import qemuInstance
 
-from afrl_zcu106_gui.qemumachinelist import qemuMachineList
-from afrl_zcu106_gui.qemucpulist import qemuCpuList
 from afrl_zcu106_gui.qemudevicelist import qemuDeviceList
 from afrl_zcu106_gui.devicesettingswidget import deviceSettingsWidget
-from afrl_zcu106_gui.machinesettingswidget import machineSettingsWidget
 from afrl_zcu106_gui.diskimagewidget import diskImageWidget
 
 
@@ -41,17 +38,13 @@ class QemuLaunchWizard(QWizard):
 
     def __init__(self, parent):
         super().__init__(parent)
-        self.machineList = qemuMachineList()  # The list of machine candidates to populate dropdown with
-        self.cpuList = qemuCpuList()  # The list of cpu candidates to populate dropdown with
         self.deviceList = qemuDeviceList()  # The list of device candidates to populate dropdown with
-        # Intermediate storage for device and machine specific settings
-        self.machineSettings = []
+        # Intermediate storage for device specific settings
         self.devices = []  # The list of devices configured to launch with the QEMU instance
         self.deviceSettings = []  # A list of device settings lists corresponding to the devices list
         self.init_ui()
-        self.lastKernelDirectory = "~/"
-        self.lastAppDirectory = "~/"
         self.lastImageDirectory = "~/"
+        self.lastCfgDirectory = "~/"
 
     def init_ui(self):
         self.ui = Ui_qemuLaunchWizard()
@@ -61,46 +54,25 @@ class QemuLaunchWizard(QWizard):
         icon = QIcon()
         icon.addFile(os.path.join(RESOURCE_ROOT, "document-open.png"), QSize(),
                      QIcon.Normal, QIcon.On)
-        self.ui.kernelButton.setIcon(icon)
-        self.ui.appButton.setIcon(icon)
+        self.ui.configSelectButton.setIcon(icon)
         self.ui.imageSelectButton.setIcon(icon)
-        self.ui.kernelButton.clicked.connect(self.openKernelFileBrowser)
-        self.ui.appButton.clicked.connect(self.openAppFileBrowser)
+        self.ui.configSelectButton.clicked.connect(self.openCfgFileBrowser)
         self.ui.imageSelectButton.clicked.connect(self.openImageFileBrowser)
         self.ui.modImagePushButton.clicked.connect(self.launchDiskImageWidget)
-        self.ui.boardSettings_PushButton.clicked.connect(self.openMachineSettings)
         self.ui.addDevicePushButton.clicked.connect(self.openDeviceSettings)
         self.ui.removeDevicePushButton.clicked.connect(self.removeDevice)
         self.ui.editDevicePushButton.clicked.connect(self.editDevice)
         self.button(QWizard.FinishButton).clicked.connect(self.launchQemuInstance)
 
-        #Setup slots to manage smp entry
-        self.ui.smpAllCheckBox.stateChanged.connect(self.checkSmpState)
-        self.ui.smpLineEdit.textChanged.connect(self.checkSmpText)
 
         # Configure the dropdown menus
-        self.initMachineDropdown()
-        self.initCpuDropdown()
         self.initDeviceTypeDropdown()
-        # Monitor machine selection to disable CPU selection if machine is selected
-        self.ui.machineComboBox.currentTextChanged.connect(self.setCpuSelectionStatus)
 
         # Register ui fields
         self.ui.qemuLaunchWizardNamePage.registerField(
             "instanceName*", self.ui.nameLineEdit)
         self.ui.qemuLaunchWizardNamePage.registerField(
             "description", self.ui.descriptionPlainTextEdit, "plainText")
-        self.ui.qemuLaunchWizardMachineCpuPage.registerField(
-            "machine", self.ui.machineComboBox)
-        self.ui.qemuLaunchWizardMachineCpuPage.registerField(
-            "cpu", self.ui.cpuComboBox)
-        self.ui.smpLineEdit.setValidator(QIntValidator(1,256))
-        self.ui.qemuLaunchWizardMachineCpuPage.registerField(
-            "smp", self.ui.smpLineEdit)
-        self.ui.qemuLaunchWizardMachineCpuPage.registerField(
-            "smpAll", self.ui.smpAllCheckBox)
-        self.ui.qemuLaunchWizardMachineCpuPage.registerField(
-            "memory", self.ui.memorySpinBox)
         self.ui.qemuLaunchWizardImagePage.registerField(
             "image*", self.ui.imageLineEdit)
         self.ui.qemuLaunchWizardNetworkPage.registerField(
@@ -111,30 +83,13 @@ class QemuLaunchWizard(QWizard):
             "gateway", self.ui.gatewayLineEdit)
         self.ui.qemuLaunchWizardNetworkPage.registerField(
             "subnetMask*", self.ui.subnetMaskLineEdit)
-        self.ui.qemuLaunchWizardKernelAppPage.registerField(
-            "kernel*", self.ui.kernelLineEdit)
-        self.ui.qemuLaunchWizardKernelAppPage.registerField(
-            "application*", self.ui.appLineEdit)
 
-        # Setup the memory entry widget to only produce values with power of 2
-        self.lastMemoryValue = self.ui.memorySpinBox.value()
-        self.ui.memorySpinBox.valueChanged.connect(self.adjustMemoryValue)
-        self.ui.memorySpinBox.lineEdit().setReadOnly(True)
-
-    def openKernelFileBrowser(self):
+    def openCfgFileBrowser(self):
         (filename, dir) = self.openFileBrowser(
-                        "Open Kernel File", ["Bin files (*.bin)",
-                        "All files (*)"], self.lastKernelDirectory, )
-        self.ui.kernelLineEdit.setText(filename)
-        self.lastKernelDirectory = dir
-        print("Setting last kernel directory to " + self.lastKernelDirectory)
-
-    def openAppFileBrowser(self):
-        (filename, dir) = self.openFileBrowser(
-                        "Open Application File", ["All files (*)"], self.lastAppDirectory)
-        self.ui.appLineEdit.setText(filename)
-        self.lastAppDirectory = dir
-        print("Setting last kernel directory to " + self.lastKernelDirectory)
+                        "Open Config File", QEMU_CFG_FILTERS, self.lastCfgDirectory)
+        self.ui.configLineEdit.setText(filename)
+        self.lastCfgDirectory = dir
+        print("Setting last kernel directory to " + self.lastCfgDirectory)
 
     def openImageFileBrowser(self):
         (filename, dir) = self.openFileBrowser(
@@ -158,19 +113,6 @@ class QemuLaunchWizard(QWizard):
             filename = fd.selectedFiles()[0]  #Single Selection mode, only one file returned in list
             dir = os.path.dirname(filename)
             return (filename, dir)
-
-
-    def initMachineDropdown(self):
-        for idx in range(0, len(self.machineList)):
-            self.ui.machineComboBox.addItem(self.machineList[idx].argument())
-            self.ui.machineComboBox.setItemData(idx, self.machineList[idx].description(), role=Qt.ToolTipRole)
-
-    def initCpuDropdown(self):
-        self.ui.cpuComboBox.addItems(self.cpuList.argumentList())
-        self.ui.cpuComboBox.activated.connect(self.selectCpu)
-
-    def selectCpu(self, idx):
-        print(f"Selected CPU is: {self.cpuList[idx].argument()}")
 
     def initDeviceTypeDropdown(self):
         self.ui.deviceTypeComboBox.addItems(self.deviceList.deviceTypeList())
@@ -208,65 +150,11 @@ class QemuLaunchWizard(QWizard):
     def editDevice(self):
         ''' Opens the device settings menu for the selected device from the device list '''
 
-    def openMachineSettings(self):
-        '''Populates a form for configuring device settings '''
-        machineStr = self.ui.machineComboBox.currentText()
-        self.machineSettingsWidget = machineSettingsWidget(machineStr)
-        self.machineSettingsWidget.settingsSignal.connect(self.applyMachineSettings)
-        self.machineSettingsWidget.show()
-
-    @Slot(list)
-    def applyMachineSettings(self, settings):
-        '''slot to gather and save the machine settings strings '''
-        print(f"MACHINE SETTINGS: {settings}")
-        self.machineSettings = settings
-
-    @Slot(str)
-    def setCpuSelectionStatus(self, text):
-        if text != "":
-            self.ui.cpuComboBox.setEnabled(False)
-            self.ui.cpuComboBox.setCurrentIndex(0)  # Clear out any CPU setting, idx 0 is default/empty
-            self.ui.cpuSettings_PushButton.setEnabled(False)
-        else:
-            self.ui.cpuComboBox.setEnabled(True)
-            self.ui.cpuSettings_PushButton.setEnabled(True)
-
-    @Slot(int)
-    def adjustMemoryValue(self, value):
-        '''slot to adjust the memory value to a power of 2 on change'''
-        print(f"Memory Changing, recieved value {value}")
-        if value > self.lastMemoryValue:
-            self.ui.memorySpinBox.setValue(self.lastMemoryValue * 2)
-        else:
-            self.ui.memorySpinBox.setValue(self.lastMemoryValue / 2)
-        self.lastMemoryValue = self.ui.memorySpinBox.value()
-
-    @Slot(bool)
-    def checkSmpState(self, value):
-        '''checks that smpLineEdit is cleared if all is checked'''
-        if self.ui.smpAllCheckBox.isChecked():
-            self.ui.smpLineEdit.clear()
-
-    @Slot(str)
-    def checkSmpText(self, value):
-        '''If text is entered in SMP line edit, clear the checkbox'''
-        if self.ui.smpLineEdit.text() != "":
-            self.ui.smpAllCheckBox.setChecked(False)
-
     def launchQemuInstance(self):
         '''Verify QEMU model data and launch instance'''
         qemu = qemuInstance()
         qemu.name = self.ui.qemuLaunchWizardNamePage.field("instanceName")
         qemu.description = self.ui.qemuLaunchWizardNamePage.field("description")
-        qemu.machine = self.machineList[self.ui.qemuLaunchWizardMachineCpuPage.field("machine")].argument()
-        qemu.machineSettings = self.machineSettings
-        qemu.cpu = self.cpuList[self.ui.qemuLaunchWizardMachineCpuPage.field("cpu")].argument()
-        if self.ui.qemuLaunchWizardMachineCpuPage.field("smpAll"):
-            qemu.smpCores = "ALL"
-        else:
-            qemu.smpCores = self.ui.qemuLaunchWizardMachineCpuPage.field("smp")
-        qemu.smpAll = self.ui.qemuLaunchWizardMachineCpuPage.field("smpAll")
-        qemu.memory = self.ui.qemuLaunchWizardMachineCpuPage.field("memory")
         qemu.devices = self.devices
         qemu.deviceSettings = self.deviceSettings
         qemu.imageName = self.ui.qemuLaunchWizardImagePage.field("image")
@@ -275,7 +163,5 @@ class QemuLaunchWizard(QWizard):
         qemu.subnetMask.setAddress(self.ui.qemuLaunchWizardNetworkPage.field("subnetMask"))
         if self.ui.qemuLaunchWizardNetworkPage.field("gateway") != "":
             qemu.gateway.setAddress(self.ui.qemuLaunchWizardNetworkPage.field("gateway"))
-        qemu.kernel = self.ui.qemuLaunchWizardKernelAppPage.field("kernel")
-        qemu.application = self.ui.qemuLaunchWizardKernelAppPage.field("application")
         print("Created QEMU Instance: ", repr(qemu))
         self.newQemuSignal.emit(qemu)
