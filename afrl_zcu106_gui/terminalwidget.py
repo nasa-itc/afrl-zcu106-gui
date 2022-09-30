45ddd3bcd6f57570f56c34b8f1ec03042c522d34
B.J. Knabenshue <benjamin.j.knabenshue@nasa.gov>
2022-09-30 11:58:43 -0400

    updated view of terminal to fill out the tab widget, updated to allow cmd to be specified to support docker container attachment

--- afrl_zcu106_gui/terminalwidget.py
+++ afrl_zcu106_gui/terminalwidget.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2009 - 2022 National Aeronautics and Space Administration. All Foreign Rights are Reserved to the U.S. Government.
 from PyQt5.QtWidgets import QWidget
-from PyQt5.QtCore import QProcess
+from PyQt5.QtCore import QProcess, QProcessEnvironment
 
 
 class terminalWidget(QWidget):
@@ -15,13 +15,28 @@ class terminalWidget(QWidget):
         print("terminalWidget Destructor")
         self.stopXterm()
 
-    def startXterm(self):
+    def startXterm(self, name="", cmd="./test.sh"):
         geo = self.size()
         print("Terminal Geo: "+str(geo))
+
+# This block sets xterm env but that gets wiped when bash starts
+#        env = QProcessEnvironment.systemEnvironment()
+#        print(f"env before change:{env.toStringList()}")
+#        env.remove("PS1")
+#        env.insert("PS1",f"{name}$")
+#        print(f"env after change:{env.toStringList()}")
+#        self.termProcess.setProcessEnvironment(env)
+
         argList = ["-into", str(int(self.winId())),
                    "-xrm", "'xterm*allowWindowOps: true'",
                    "-fg", "green",
-                   "-fullscreen"]
+                   "-aw",
+                   "+fullscreen",
+                   "-hold",
+                   "-maximized",]
+        if cmd:
+            argList.extend(["-e",cmd, ])
+
         print("xterm arglist: "+str(argList))
         self.termProcess.start("xterm", argList)
         print("Terminal Started, PID: " + str(self.termProcess.processId()))
