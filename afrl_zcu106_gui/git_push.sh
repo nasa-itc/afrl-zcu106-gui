028e14cb5ac0dc73c6ff6942bc8f8eeb71ca0c4b
B.J. Knabenshue <benjamin.j.knabenshue@nasa.gov>
2022-09-30 11:59:54 -0400

    updates to gui

---
+++ afrl_zcu106_gui/git_push.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+git push https://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@github.com/nasa-itc/afrl-zcu106-gui.git
---
+++ afrl_zcu106_gui/test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export PS1=test$
+echo "This is the test script for xterm"
+/bin/bash
