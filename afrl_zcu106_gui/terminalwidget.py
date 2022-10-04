4a34ab8c902dfeabd5e076b0db79d76ece1f75e6
B.J. Knabenshue <benjamin.j.knabenshue@nasa.gov>
2022-10-04 14:53:49 -0400

    cleaned up, added in smartish approach to getting terminal size correct

--- afrl_zcu106_gui/terminalwidget.py
+++ afrl_zcu106_gui/terminalwidget.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2009 - 2022 National Aeronautics and Space Administration. All Foreign Rights are Reserved to the U.S. Government.
-from PyQt5.QtWidgets import QWidget
-from PyQt5.QtCore import QProcess, QProcessEnvironment
+from PyQt5.QtWidgets import QWidget, QScrollArea
+from PyQt5.QtCore import QProcess
 
 
 class terminalWidget(QWidget):
@@ -9,7 +9,7 @@ class terminalWidget(QWidget):
         QWidget.__init__(self)
         # super().__init__(self)
         # self.termProcess = QProcess(self.parent())
-        self.termProcess = QProcess(self)
+        self.termProcess = QProcess()
 
     def __del__(self):
         print("terminalWidget Destructor")
@@ -17,27 +17,18 @@ class terminalWidget(QWidget):
 
     def startXterm(self, name="", cmd="./test.sh"):
         geo = self.size()
-        print("Terminal Geo: "+str(geo))
-
-# This block sets xterm env but that gets wiped when bash starts
-#        env = QProcessEnvironment.systemEnvironment()
-#        print(f"env before change:{env.toStringList()}")
-#        env.remove("PS1")
-#        env.insert("PS1",f"{name}$")
-#        print(f"env after change:{env.toStringList()}")
-#        self.termProcess.setProcessEnvironment(env)
-
+        cols = int(geo.width()/6) #Estimate xterm geometry from widget size, 6,14 determined by observation
+        lines=int(geo.height()/14)
+        print(f"starting terminal size {cols}x{lines}")
         argList = ["-into", str(int(self.winId())),
-                   "-xrm", "'xterm*allowWindowOps: true'",
-                   "-fg", "green",
+                   #"-xrm", "'xterm*allowWindowOps: true'",
                    "-aw",
-                   "+fullscreen",
                    "-hold",
-                   "-maximized",]
+                   "-geometry",
+                   f"{cols}x{lines}"]
         if cmd:
             argList.extend(["-e",cmd, ])
 
-        print("xterm arglist: "+str(argList))
         self.termProcess.start("xterm", argList)
         print("Terminal Started, PID: " + str(self.termProcess.processId()))
         return self.termProcess.processId()
